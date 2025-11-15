c0331cb7671dc54928189ce9e313c91a4cca52c4
Joel Pinto <joelsmpinto@outlook.com>
2025-11-15 14:10:48 +0000

    fix(tests): Fixed test backend url configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {assertApiConfig} from "@/api/api";
+
+assertApiConfig();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
