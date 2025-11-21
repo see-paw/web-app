1b7263f28e5f1532efbceae0ed72a31bde24d1ad
Joel Pinto <joelsmpinto@outlook.com>
2025-11-21 09:55:39 +0000

    feat(auth): Added AuthStore and User Types

---
+++ src/stores/auth.store.ts
@@ -0,0 +1,16 @@
+﻿import {createStore} from "zustand/vanilla";
+import type {AuthResponse, User} from "@/types/user";
+
+type AuthState = {
+    user: User | null
+    tokens: AuthResponse | null
+    login: (data: { user: User; tokens: AuthResponse }) => void
+    logout: () => void
+}
+
+export const useAuthStore = createStore<AuthState>((set) =>({
+    user: null,
+    tokens: null,
+    login: ({ user, tokens }) => set({ user, tokens }),
+    logout: () => set({ user: null, tokens: null }),
+}))
---
+++ src/types/user.ts
@@ -0,0 +1,34 @@
+﻿export interface AuthResponse {
+    tokenType: string
+    accessToken: string
+    expiresIn: number
+    refreshToken: string
+}
+
+export interface UserId {
+    userId: string
+}
+
+export interface UserRole {
+    role: string
+}
+
+export interface UserProfile {
+    name: string
+    birthDate: string
+    street: string
+    city: string
+    postalCode: string
+    phoneNumber: string
+}
+
+export interface User {
+    userId: string,
+    role: string
+    profile: UserProfile
+}
+
+export interface AuthTokens {
+    accessToken: string
+    refreshToken: string
+}
